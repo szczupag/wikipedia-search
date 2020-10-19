3ae9e491b2452c2529e3833e2484d49615bada8a
Agata Szczuka <agata.szczuka@gmail.com>
2020-10-19 11:38:59 +0200

    Create search request on button click.

--- src/components/SearchPanel/index.js
+++ src/components/SearchPanel/index.js
@@ -2,9 +2,40 @@ import React, { useState } from 'react';
 import Button from '../Button';
 import Input from '../Input';
 
+const urlWithParamsBuilder = (baseURL, data) => {
+  const searchParams = new URLSearchParams(data);
+  const url = new URL(baseURL);
+  url.search = searchParams.toString();
+  return url;
+};
+
+const requestWikipediaQuery = async (searchPhrase) => {
+  const requestParams = {
+    action: 'query',
+    list: 'search',
+    format: 'json',
+    srlimit: '10',
+    origin: '*',
+    srsearch: searchPhrase,
+  };
+  const url = urlWithParamsBuilder('https://en.wikipedia.org/w/api.php', requestParams);
+  const response = await fetch(url);
+  const data = await response.json();
+  if (!response.ok) {
+    throw(`[Wikipedia API error] ${response.status}`);
+  }
+  const result = (data.query || {}).search || [];
+  if (result.length === 0) {
+    throw('No result found!')
+  }
+  return result;
+};
+
 const SearchPanel = () => {
   const [searchPhrase, setSearchPhrase] = useState('');
   const [replaceWith, setReplaceWith] = useState('');
+  const [results, setResults] = useState([]);
+  const [errors, setErrors] = useState(false);
 
   const inputChangeHandler = (event, setValue) => {
     event.persist();
@@ -12,7 +43,16 @@ const SearchPanel = () => {
     setValue(value);
   };
 
-  const searchButtonClickHandler = () => console.log('Search!', searchPhrase);
+  const requestWikipediaQueryHandler = () => {
+    requestWikipediaQuery(searchPhrase)
+      .then(setResults)
+      .catch(setErrors)
+  }
+
+  const searchButtonClickHandler = () => {
+    requestWikipediaQueryHandler();
+  };
+
   const replaceButtonClickHandler = () => console.log('Replace!', replaceWith);
   const replaceAllButtonClickHandler = () => console.log('Replace all!', replaceWith);
 
@@ -40,6 +80,10 @@ const SearchPanel = () => {
         text="Replace all"
         onClick={replaceAllButtonClickHandler}
       />
+      <p>Results</p>
+      {results.map(el => <p>{JSON.stringify(el)}</p>)}
+      <p>Errors</p>
+      {errors}
     </div>
   );
 }
